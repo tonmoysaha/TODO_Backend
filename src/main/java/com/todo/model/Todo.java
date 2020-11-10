ade34e76e83de102f990a8c381aac538c2d02873
tonmoysaha <tonmoysahaopi@gmail.com>
2020-11-09 19:13:00 -0800

    add database h2 data

--- src/main/java/com/todo/model/Todo.java
+++ src/main/java/com/todo/model/Todo.java
@@ -2,8 +2,17 @@ package com.todo.model;
 
 import java.util.Date;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String username;
 	private String description;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'opi', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'opi', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'opi', 'Learn Microservices', sysdate(), false);
